2013c8ce8a05aa301c3371be92bb3d87b1198854
Rick Bakker <rickbakkr@gmail.com>
2026-06-22 16:29:25 +0200

    Update zehnder.h

--- components/zehnder/zehnder.h
+++ components/zehnder/zehnder.h
@@ -19,10 +19,7 @@ namespace zehnder {
 /* Fan device types */
 enum {
   FAN_TYPE_BROADCAST = 0x00,       // Broadcast to all devices
-  
-  // WHR 930 identificeert zich als 0x12, niet 0x01
-  FAN_TYPE_MAIN_UNIT = 0x12,       // Fans
-
+  FAN_TYPE_MAIN_UNIT = 0x01,       // Fans
   FAN_TYPE_REMOTE_CONTROL = 0x03,  // Remote controls
   FAN_TYPE_CO2_SENSOR = 0x18
 };  // CO2 sensors
